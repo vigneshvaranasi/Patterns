96e2d8141ac5ac0cd1937f10e110432f47fe4c68
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-01-15 19:02:49 +0530

    finish 10 in triangles

---
+++ Triangle Patterns/10-Pattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number of Rows: ";
+    cin>>n;
+    cout<<endl;
+    int i,j,k;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+            cout<<" ";
+        for(k=0;k<=n-i-1;k++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i;j++)
+            cout<<" ";
+        for(k=0;k<i+1;k++)
+            cout<<"* ";
+        cout<<endl;
+    }
+}
