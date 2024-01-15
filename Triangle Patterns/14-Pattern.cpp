e9ca63fb9e32054f806270567863848ce4ec5863
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-01-15 22:33:11 +0530

    Finish triangle Patterns

---
+++ Triangle Patterns/14-Pattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number of Rows: ";
+    cin>>n;
+    int i,j,k;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+            cout<<"* ";
+        for(j=0;j<2*(n-i-1);j++)
+            cout<<"  ";
+        for(k=0;k<=i;k++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+            cout<<"* ";
+        for(int j=0;j<2*i;j++)
+            cout<<"  ";
+        for(int k=0;k<n-i;k++)
+            cout<<"* ";
+        cout<<endl;
+    }
+
+}
