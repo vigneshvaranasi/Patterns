57cc00a5a0171d55f2d7b20245d0408c7a605190
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-01-16 09:01:07 +0530

    Finish 7 in numeric

---
+++ Numeric Patterns/07-Pattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number of Rows: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int k=i+1;
+        for(int j=0;j<n-i;j++,k++)
+            cout<<k<<" ";
+        cout<<endl;
+    }
+    for(int i=n-2;i>=0;i--)
+    {
+        int k=i+1;
+        for(int j=0;j<n-i;j++,k++)
+            cout<<k<<" ";
+        cout<<endl;
+    }
+}
