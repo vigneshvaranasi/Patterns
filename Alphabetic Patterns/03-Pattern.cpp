f32c095d58d7af99005c12090384e0627f4ed959
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-01-31 07:52:14 +0530

    Finish Alphabetic Patterns

---
+++ Alphabetic Patterns/03-Pattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Number of Rows: ";
+    cin >> n;
+    int k = 1;
+    char ch = 'A' + n - 1;
+    for (int i = 0; i < n; i++)
+    {
+        char c = ch - i;
+        for (int j = 0; j <n-i; j++, k++)
+        {
+            cout << c << " ";
+            c--;
+        }
+        cout << endl;
+    }
+}
