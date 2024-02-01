76f42aa4e6035ea218e1834c00a49fa31079301e
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2024-02-01 09:31:18 +0000

    should finish 3 in spl

---
+++ Special Patterns/03-Pattern.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number of Rows: ";
+    cin>>n;
+    int i,j,k;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=n-i;j++)
+            cout<<" ";
+        for(k=0;k<i+1;k++)
+        {
+            if(k==0 ||k==i)
+                cout<<"* ";
+            else
+                cout<<"  ";
+        }
+        for(j=0;j<=n-i-2;j++)
+            cout<<"  ";
+        for(k=0;k<i+1;k++)
+        {
+            if(k==0 ||k==i)
+                cout<<"* ";
+            else
+                cout<<"  ";
+        }
+        cout<<endl;
+    }
+
+}
